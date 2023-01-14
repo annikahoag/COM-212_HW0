19e302644e447030415c5f811d3d78c33413e96b
annikahoag <71946573+annikahoag@users.noreply.github.com>
2023-01-14 12:21:39 -0500

    Add files via upload

---
+++ Universe.java
@@ -0,0 +1,8 @@
+public class Universe{
+	public static void main(String[]args){
+
+		String name = args[0];
+		String name2 = args[1];
+		System.out.println("Hello " + name + " and " + name2 + "!");
+	}
+}
